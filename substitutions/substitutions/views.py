3b608081f5e0eac4ee7aa40652764633bbe9d422
Mandy Tensen <mandy@pushcollective.net>
2014-12-12 00:19:46 -0600

    feature: listing page template
    - Add logo and dummy product image
    - Add new CSS file
    - move all CSS files into css directory
    - create listing page template in my template.pt
    - added dummy placeholder products to to database

--- substitutions/substitutions/views.py
+++ substitutions/substitutions/views.py
@@ -14,18 +14,87 @@ def index(request):
     items = [
         {
             'title': 'Pizza',
-            'url': 'http://www.pizzamarket.net/images/pizza2.jpg',
+            'dollars': '3',
+            'cents': '88',
+            'img': 'static/images/digiorno-pizza.jpg',
             'id': '1'
         },
         {
             'title': 'Bread',
-            'url': 'http://www.pizzamarket.net/images/pizza2.jpg',
+            'dollars': '3',
+            'cents': '88',
+            'img': 'static/images/digiorno-pizza.jpg',
             'id': '2'
         },
         {
             'title': 'Milk',
-            'url': 'http://www.pizzamarket.net/images/pizza2.jpg',
+            'dollars': '3',
+            'cents': '88',
+            'img': 'static/images/digiorno-pizza.jpg',
             'id': '3'
+        },
+        {
+            'title': 'Pizza',
+            'dollars': '3',
+            'cents': '88',
+            'img': 'static/images/digiorno-pizza.jpg',
+            'id': '4'
+        },
+        {
+            'title': 'Bread',
+            'dollars': '3',
+            'cents': '88',
+            'img': 'static/images/digiorno-pizza.jpg',
+            'id': '5'
+        },
+        {
+            'title': 'Milk',
+            'dollars': '3',
+            'cents': '88',
+            'img': 'static/images/digiorno-pizza.jpg',
+            'id': '6'
+        },
+        {
+            'title': 'Pizza',
+            'dollars': '3',
+            'cents': '88',
+            'img': 'static/images/digiorno-pizza.jpg',
+            'id': '7'
+        },
+        {
+            'title': 'Bread',
+            'dollars': '3',
+            'cents': '88',
+            'img': 'static/images/digiorno-pizza.jpg',
+            'id': '8'
+        },
+        {
+            'title': 'Milk',
+            'dollars': '3',
+            'cents': '88',
+            'img': 'static/images/digiorno-pizza.jpg',
+            'id': '9'
+        },
+        {
+            'title': 'Pizza',
+            'dollars': '3',
+            'cents': '88',
+            'img': 'static/images/digiorno-pizza.jpg',
+            'id': '10'
+        },
+        {
+            'title': 'Bread',
+            'dollars': '3',
+            'cents': '88',
+            'img': 'static/images/digiorno-pizza.jpg',
+            'id': '11'
+        },
+        {
+            'title': 'Milk',
+            'dollars': '3',
+            'cents': '88',
+            'img': 'static/images/digiorno-pizza.jpg',
+            'id': '12'
         }
     ]
 
