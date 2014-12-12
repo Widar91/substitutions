a322079122ee60e7b3bfad0e24348da4b6be192f
Bryan Dilks <bdilks@l-sb8t1wpfd5-m.homeoffice.wal-mart.com>
2014-12-11 17:05:07 -0800

    removed database requirement

--- substitutions/substitutions/views.py
+++ substitutions/substitutions/views.py
@@ -11,11 +11,6 @@ from .models import (
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-
     items = [
         {
             'title': 'Pizza',
